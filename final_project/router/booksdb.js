4efb5b2037428de7ff7cd94458b3528e43f1a468
jessekorff <jessekorff@gmail.com>
2025-12-05 14:58:30 -0500

    add routes to search by title, author, isbn, and reviews by isbn

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "ISBN": 123456789 },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "ISBN": 234567891 },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "ISBN": 345678912 },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "ISBN": 456789123 },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "ISBN": 567891234 },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "ISBN": 678912345 },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "ISBN": 78912345 },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "ISBN": 891234567 },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "ISBN": 912345678 },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "ISBN": 987654321 }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "isbn": "123456789" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "isbn": "234567891" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": "345678912" },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "isbn": "456789123" },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "isbn": "567891234" },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "isbn": "678912345" },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "isbn": "78912345" },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "isbn": "891234567" },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "isbn": "912345678" },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "isbn": "987654321" }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,26 +18,65 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  // Retrieve the ISBN parameter from the request URL and send the corresponding book details
+  const isbn = req.params.isbn;
+
+  // Find book by searching through all books
+  const book = Object.values(books).find(b => b.isbn === isbn);
+
+  if (book) {
+    return res.json(book);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+    });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  //Retrieve the author parameter from the request URL and send book details
+  const author = req.params.author;
+
+  // Find book by searching through all books
+  const book = Object.values(books).filter(b => b.author === author);
+
+  if (book.length > 0) {
+    return res.json(book);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+    });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  //Retrieve the title parameter from the request URL and send book details
+  const title = req.params.title;
+
+  // Find book by searching through all books
+  const book = Object.values(books).filter(b => 
+    b.title.toLowerCase().includes(title.toLowerCase())
+  );
+
+  if (book.length > 0) {
+    return res.json(book);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+    });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    //Retrieve the title parameter from the request URL and send book details
+    const isbn = req.params.isbn;
+
+    // Find book by searching through all books
+    const book = Object.values(books).find(b => b.isbn === isbn);
+
+    if (book) {
+    return res.json(book.reviews);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+  });
 
 module.exports.general = public_users;
