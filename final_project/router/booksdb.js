66138b437de4f5a30a115b524dadbbb5cd26e414
jessekorff <jessekorff@gmail.com>
2025-12-05 13:43:23 -0500

    GET path works

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "ISBN": 123456789 },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "ISBN": 234567891 },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "ISBN": 345678912 },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "ISBN": 456789123 },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "ISBN": 567891234 },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "ISBN": 678912345 },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "ISBN": 78912345 },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "ISBN": 891234567 },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "ISBN": 912345678 },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "ISBN": 987654321 }
 }
 
 module.exports=books;
