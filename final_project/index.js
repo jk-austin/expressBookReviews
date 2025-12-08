b80be9bf8e0549ea7feb1c64a9e2bf43f88b2d30
jessekorff <jessekorff@gmail.com>
2025-12-08 12:38:09 -0500

    add endpoints for register & login in auth_users.js

--- final_project/index.js
+++ final_project/index.js
@@ -9,33 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
-// Login endpoint
-app.post("/customer/login", function auth(req,res,next){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // Check if username or password is missing
-    if (!username || !password) {
-        return res.status(400).json({ message: "Error logging in" });
-    }
-
-    // Authenticate user
-    if (authenticatedUser(username, password)) {
-        // Generate JWT access token
-        let accessToken = jwt.sign({
-            data: password
-        }, 'access', { expiresIn: 60 * 60 });
-
-        // Store access token and username in session
-        req.session.authorization = {
-            accessToken, username
-        }
-        return res.status(200).send("User successfully logged in");
-    } else {
-        return res.status(401).json({ message: "Invalid Login. Check username and password" });
-    }
-});
  
 const PORT =5000;
 
